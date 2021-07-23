3b35d13c11a24b3af7d59442dbb091bb6f783456
FelipePereiraBL <felipereirabl@gmail.com>
2021-07-23 15:50:49 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListeners;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exceptions.ValidationException;
 import model.services.DepartmentServices;
 
 public class DepartmentFormController implements Initializable
@@ -74,6 +77,10 @@ public class DepartmentFormController implements Initializable
 			notifyChangeListeners();
 			Utils.currentStage(event).close();
 		}
+		catch(ValidationException e)
+		{
+			setErrorMessages(e.getErrors());
+		}
 		catch (DbException e) 
 		{
 			Alerts.showAlerts("Error saving object", null, e.getMessage(), AlertType.ERROR);
@@ -96,7 +103,21 @@ public class DepartmentFormController implements Initializable
 		Department obj=new Department();
 		
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+		
+		ValidationException exception=new ValidationException("Validate Exception");
+		
+		if(txtName.getText()==null||txtName.getText().trim().equals(""))
+		{
+			exception.addErrors("name", "Field can´t be empty");
+		}
+		
 		obj.setName(txtName.getText());
+		
+		if(exception.getErrors().size()>0)
+		{
+			throw exception;
+		}
+		
 		return obj;
 	}
 	@FXML
@@ -125,6 +146,17 @@ public class DepartmentFormController implements Initializable
 		}
 		txtId.setText(String.valueOf(entity.getId()));
 		txtName.setText(entity.getName());
+	}
+	
+	private void setErrorMessages(Map<String,String>errors)
+	{
+		Set<String>fields=errors.keySet();
+		
+		if(fields.contains("name"))
+		{
+			labelErrorName.setText(errors.get("name"));
+		}
+		
 	}
 
 }
